764cba25f1dfa26a7a6c3af4e8875f3d398ecf19
acaty64 <ana.arashiro@gmail.com>
2017-12-18 04:23:06 +0000

    Lesson 3 - Diseño de un componente

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    public function getIdeas()
+    {
+    	return Idea::orderBy('id', 'DESC')->get();
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'description' => 'required'
+    	]);
+
+    	Idea::create($request->all());
+
+    	return;
+    }
+
+
+}
